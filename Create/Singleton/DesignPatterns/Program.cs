7e9eebfeb13822f603174efbb4f8825589a4719a
sam <samliao1@auforce.com>
2024-08-13 12:24:38 +0800

    ✨ feat: 新增 Singleton 的範例程式

---
+++ Create/Singleton/DesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesignPatterns;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 獲取 Singleton 實例並呼叫方法
+        Singleton.Instance.SomeMethod();
+    }
+}
---
+++ Create/Singleton/DesignPatterns/Singleton.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        // 靜態變數來保存類別的唯一實例
+        private static readonly Singleton instance = new();
+
+        // 私有建構函數，防止外部實例化
+        private Singleton()
+        {
+        }
+
+        // 公共靜態方法來獲取唯一實例
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        // 其他成員方法
+        public void SomeMethod()
+        {
+            // 實作一些功能
+            Console.WriteLine("Singleton instance method called.");
+        }
+    }
+}
