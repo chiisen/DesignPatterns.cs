28fde1e5cf4feff85eb8666bd76afe9d6cd85355
sam <samliao1@auforce.com>
2024-08-13 13:08:00 +0800

    ✨ feat: 新增 Composite 的範例程式

---
+++ Structure/Composite/DesignPatterns/Component/Component.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Component
+{
+    /// <summary>
+    /// Component
+    /// </summary>
+    abstract class Component
+    {
+        protected string name;
+
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Display(int depth);
+    }
+}
---
+++ Structure/Composite/DesignPatterns/Component/Leaf.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Component
+{
+    /// <summary>
+    /// Leaf
+    /// </summary>
+    class Leaf : Component
+    {
+        public Leaf(string name) : base(name) { }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+    }
+}
